e25b1ccaf7037be8153e94f6c7462f2c6a3940c1
Joseph Stewart <stewartjoseph255@gmail.com>
2023-04-17 20:19:51 -0600

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(person, personsEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({ 
+            name: person,
+            email: personsEmail,
+        }),
+    })
+        .then(resp => resp.json())
+        .then(function(json) {
+            document.body.innerHTML = json.id;
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message;
+        })
+    }
